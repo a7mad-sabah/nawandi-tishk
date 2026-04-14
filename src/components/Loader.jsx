14a463ecaa0f45373673d48e12d1d77d57a4c72c
Ahmad <amadsabah6@gmail.com>
2026-04-14 13:47:48 +0300

    tawawa

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,12 +3,12 @@ const Loader = () => {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-white z-50">
       <img
-        src="./src/assets/book.gif" // 👈 ئەگەر لە public داناوە
+        src="/images/book.gif" // 👈 ئەگەر لە public داناوە
         alt="loading"
         className="w-24 h-24 object-contain"
       />
     </div>
   );
 };
-
+// هه
 export default Loader;
